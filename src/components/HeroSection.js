dddec37c13de8f275fd0ef3d93118dcce6ab295a
ozee1z <aanyehschlink@yahoo.com>
2023-08-30 17:19:47 +0000

    made changes to HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,7 +11,7 @@ export default function HeroSection () {
     return (
         <div className="container hero-section d-flex justify-content-between" id="section1" style={{marginTop: '100px'}}>
         <div>
-            <a href="#" className="link-underline link-underline-opacity-0 link-opacity-100-hover"><i className="bi bi-currency-bitcoin border border-primary rounded-circle bg-primary text-white me-2"></i>Start your portfolio<i className="ms-2" ></i><ArrowRight/></a>
+            <a href="#section1" className="link-underline link-underline-opacity-0 link-opacity-100-hover"><i className="bi bi-currency-bitcoin border border-primary rounded-circle bg-primary text-white me-2"></i>Start your portfolio<i className="ms-2" ></i><ArrowRight/></a>
             <div className="mt-3">
                 <h1 className="fw-bold lh-base">Buy, Sell and Trade<br /> Your Cryto</h1>
                 <p className="fw-semibold lh-base">Bit market is the easiest place to buy and sell<br /> cryptocurrency. Sign up and get started today.</p>
